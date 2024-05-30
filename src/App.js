8ace32e8d7403644484b0a18daa72f77531da450
Dileep Jose <dileepj@gmail.com>
2024-05-30 09:19:11 -0600

    Added Name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Dileep. Welcome to React
+          Hello Dileep Jose. Welcome to React
         </p>
         <a
           className="App-link"
